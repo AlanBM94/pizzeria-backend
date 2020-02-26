10ffa998629ce5580f0de9cfd745afe27f331483
Alan <sevilbm94@hotmail.com>
2020-02-26 09:14:35 -0600

    Update and delete comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -45,7 +45,7 @@ module.exports.findCommentsByUser = async (req, res, next) => {
   try {
     comments = await Comment.find({ user: userId });
   } catch (error) {
-    return next(new HttpError("Could not find comments by that user", 500));
+    return next(new HttpError("Could not find comments by that user", 404));
   }
 
   res.status(200).json({
@@ -60,10 +60,51 @@ module.exports.findCommentsByFood = async (req, res, next) => {
   try {
     comments = await Comment.find({ food: foodId });
   } catch (error) {
-    return next(new HttpError("Could not find comments by that food", 500));
+    return next(new HttpError("Could not find comments by that food", 404));
   }
 
   res.status(200).json({
     comments
   });
+};
+
+const findComment = async (commentId, next) => {
+  let updatedComment;
+  try {
+    updatedComment = await Comment.findById(commentId);
+    return updatedComment;
+  } catch (error) {
+    return next(new HttpError("Could not find comment with that id", 404));
+  }
+};
+
+module.exports.updateComment = async (req, res, next) => {
+  const { comment } = req.body;
+  const commentId = req.params.commentId;
+  const updatedComment = await findComment(commentId, next);
+  if (!updatedComment)
+    return next(new HttpError("Could not find a comment for that id", 404));
+
+  updatedComment.comment = comment;
+
+  try {
+    await updatedComment.save();
+  } catch (error) {
+    return next(new HttpError("Could not update comment", 500));
+  }
+
+  res.status(200).json({
+    comment: updatedComment
+  });
+};
+
+module.exports.deleteComment = async (req, res, next) => {
+  const commentId = req.params.commentId;
+  const deletedComment = await (commentId, next);
+  if (!deletedComment) {
+    return next(new HttpError("Could not find a comment for that id", 404));
+  } else {
+    await Comment.findByIdAndRemove(commentId);
+    res.status(200).json({ message: "Deleted place" });
+  }
 };
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,16 +4,20 @@ const userController = require("../controllers/userController");
 
 const route = express.Router();
 
+route.get("/:foodId", commentController.findCommentsByFood);
+
 route.use(userController.protect);
 
 route.get("/", commentController.findCommentsByUser);
-
-route.get("/:foodId", commentController.findCommentsByFood);
 
 route.post(
   "/",
   commentController.setFoodUserIds,
   commentController.createComment
 );
+
+route.patch("/:commentId", commentController.updateComment);
+
+route.delete("/:commentId", commentController.deleteComment);
 
 module.exports = route;
